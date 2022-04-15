3ef4c3a4abaa9692a72194bb710f3c5a53f50945
rozodkarbharat <99673832+rozodkarbharat@users.noreply.github.com>
2022-04-15 12:33:56 +0530

    counter assignment

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
 
 import './App.css';
-// import Button from './day2/button';
 import Button1 from './day2/button1';
-// import Employeedetail from './day2/employeedetal';
-// import ContactUs from './day1/contact_us';
-// import Download from './day1/download';
-// import Help from './day1/help';
-// import Home from './day1/home';
-// import JoinUs from './day1/join_us';
-// import Login from './day1/login';
-// import Search from './day1/search';
-// import Setting from './day1/setting';
 
 
 
@@ -19,8 +9,6 @@ function App() {
   return (
 
    <div className="App">
-   {/* <h1 className='header'>Student's Form</h1>
-  <Employeedetail/> */}
 <Button1/>
    </div>
   )
@@ -28,14 +16,5 @@ function App() {
 
 export default App;
 
-
-  //  <JoinUs/>
-  //    <Setting/>
-  //    <Login/>
-  //   <ContactUs/>
-  //   <Search/>
-  //   <Help/>
-  //   <Home/>
-  //   <Download/>
 
 
